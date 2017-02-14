4a8e74f9bc638dd1e5432891de6281c2fb76625b
John Dupper <john.dupper@me.com>
2017-02-14 15:21:15 -0500

    stripped residual html markup from job description snippet

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,7 +11,5 @@ class SearchController < ApplicationController
   def results
     api_response = HTTParty.get("http://api.indeed.com/ads/apisearch?publisher=9447015102421242&q=#{request.POST['job_title']}&l=#{request.POST['job_location']}&sort=date&radius=&st=&jt=&start=&limit=25&fromage=30&filter=&latlong=&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json").parsed_response
     @all_jobs = api_response['results']
-    @job_title = request.POST['job_title']
-    @job_location = request.POST['job_location']
   end
 end
