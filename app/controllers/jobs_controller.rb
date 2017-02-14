9e515b3f7153e92aa1acb7da67356d68ef0d1afe
John Dupper <john.dupper@me.com>
2017-02-14 14:54:35 -0500

    crud functionality stable, redirect removed, need flash message

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,44 +3,39 @@ class JobsController < ApplicationController
   # allows for auth. bypass for local development
   skip_before_filter :verify_authenticity_token
 
-  # GET /jobs
-  # GET /jobs.json
   def index
     @jobs = Job.all
   end
 
-  # GET /jobs/1
-  # GET /jobs/1.json
   def show
   end
 
-  # GET /jobs/new
   def new
     @job = Job.new
   end
 
-  # GET /jobs/1/edit
   def edit
   end
 
-  # POST /jobs
-  # POST /jobs.json
   def create
     @job = Job.new(job_params)
 
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
-        format.json { render :show, status: :created, location: @job }
-      else
-        format.html { render :new }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    if @job.save
+      flash.now[:notice] = 'Job saved to your dashboard!'
+    else
+      flash.now[:alert] = 'Error while saving, try again?'
     end
+    # respond_to do |format|
+    #   if @job.save
+    #     format.html { render :index, notice: 'Job was successfully created.' }
+    #     format.json { render :show, status: :created, location: @job }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @job.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
-  # PATCH/PUT /jobs/1
-  # PATCH/PUT /jobs/1.json
   def update
     respond_to do |format|
       if @job.update(job_params)
@@ -53,8 +48,6 @@ class JobsController < ApplicationController
     end
   end
 
-  # DELETE /jobs/1
-  # DELETE /jobs/1.json
   def destroy
     @job.destroy
     respond_to do |format|
@@ -69,8 +62,8 @@ class JobsController < ApplicationController
       @job = Job.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Removed 'require' from params
     def job_params
-      params.require(:job).permit(:title, :company, :location, :snippet, :date)
+      params.permit(:title, :company, :location, :snippet, :date)
     end
 end
